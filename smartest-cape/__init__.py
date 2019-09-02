1edaaadea237aed8992732b29507ab4036349e52
David Cao <david@cao.st>
2019-09-02 16:00:49 -0700

    Add proper home and course listing pages

--- smartest-cape/__init__.py
+++ smartest-cape/__init__.py
@@ -4,9 +4,12 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
 
-    # a simple page that says hello
     @app.route('/')
     def root():
         return render_template('index.html')
+
+    @app.route('/listing')
+    def listing():
+        return render_template('listing.html')
 
     return app
