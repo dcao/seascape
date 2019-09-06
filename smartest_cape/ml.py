36a030d3115b84857fb0c7610da524791237eeee
David Cao <david@cao.st>
2019-09-05 17:04:38 -0700

    Fix indents and footer

--- smartest_cape/ml.py
+++ smartest_cape/ml.py
@@ -13,30 +13,30 @@ terms = {}
 curr = ""
 incr = 0
 for i in range(len(term_list)):
-	if term_list[len(term_list)-i-1] != curr:
-		curr = term_list[len(term_list)-i-1]
-		terms[curr] = incr
-		incr += 1		
+    if term_list[len(term_list)-i-1] != curr:
+        curr = term_list[len(term_list)-i-1]
+        terms[curr] = incr
+        incr += 1               
 
 
 def regress(input_prof, input_course, ylabels):
-	global dataset 
-	global terms
-	indices=[]
-	instructor = dataset["instr"]
-	course = dataset["course"]
+    global dataset 
+    global terms
+    indices=[]
+    instructor = dataset["instr"]
+    course = dataset["course"]
 
-	df = dataset.loc[(dataset["instr"] == input_prof) & (dataset["course"] == input_course)]
+    df = dataset.loc[(dataset["instr"] == input_prof) & (dataset["course"] == input_course)]
 
-	df["term"] = df["term"].apply(lambda x: terms[x])
-	x = df[["term"]]
-	y = df[ylabels]
+    df["term"] = df["term"].apply(lambda x: terms[x])
+    x = df[["term"]]
+    y = df[ylabels]
 
-	print(x)
-	print(y)
-	
-	#Fitting Simple Linear Regression to the Training set
-	regressor = MultiTaskElasticNet()
-	regressor.fit(x, y)
-	return regressor.predict([[max(terms.values()) + 1]])
+    print(x)
+    print(y)
+
+    #Fitting Simple Linear Regression to the Training set
+    regressor = MultiTaskElasticNet()
+    regressor.fit(x, y)
+    return regressor.predict([[max(terms.values()) + 1]])
 
