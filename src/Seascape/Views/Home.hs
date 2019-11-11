121185ec063e78c4d3bc3dc4f77c9968dd6a8b8f
David Cao <david@cao.st>
2019-11-11 04:33:14 -0800

    Add more to landing page

--- src/Seascape/Views/Home.hs
+++ src/Seascape/Views/Home.hs
@@ -9,4 +9,9 @@ homeView = defaultPartial "Seascape" $ do
   div_ [class_ "bg-teal-100 pt-16 pb-40"] $ do
     div_ [class_ "flex flex-col justify-center max-w-2xl mx-auto text-center"] $ do
       h1_ [class_ "text-5xl font-sans tracking-tight"] "Your virtual counselor."
-      p_ [class_ "text-xl text-serif mt-5 text-teal-600"] "Seascape provides utilities for members of the UCSD community for discovering insights and making informed course-related decisions."
+      p_ [class_ "text-xl text-serif mt-5 text-teal-600"] "Seascape provides utilities for members of the UCSD community to discover insights and make informed course-related decisions."
+      -- input_ [class_ "shadow-lg outline-none appearance-none border rounded-lg w-full py-4 px-6 text-xl text-gray-700 leading-tight", id_ "username", type_ "text", placeholder_ "Search for a class..."]
+      div_ [class_ "mt-12 flex items-center border shadow-lg rounded-lg w-full bg-white pl-6 text-xl leading-tight"] $ do
+        input_ [class_ "outline-none appearance-none border-none w-full text-gray-700", id_ "username", type_ "text", placeholder_ "Search for a class or instructor"]
+        button_ [class_ "flex-shrink-0 text-teal-500 hover:text-teal-700 p-6", type_ "button"] $ do
+          i_ [class_ "fas fa-search"] mempty
--- src/Seascape/Views/Partials.hs
+++ src/Seascape/Views/Partials.hs
@@ -16,7 +16,7 @@ path_ = term "path"
 
 navbarPartial :: Html ()
 navbarPartial = nav_ [class_ "flex items-center justify-between flex-wrap bg-teal-500 py-5 shadow-lg z-100 px-6 relative"] $ do
-  div_ [class_ "lg:mx-auto lg:container w-full justify-between flex-wrap flex items-center"] $ do
+  div_ [class_ "mx-auto container w-full justify-between flex-wrap flex items-center"] $ do
     div_ [class_ "flex items-center flex-shrink-0 text-white"] $ do
       span_ [class_ "font-semibold text-xl tracking-tight"] "Seascape"
     div_ [class_ "block lg:hidden"] $ do
@@ -26,10 +26,10 @@ navbarPartial = nav_ [class_ "flex items-center justify-between flex-wrap bg-tea
           path_ [d_ "M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"] mempty
     div_ [class_ "w-full block flex-grow lg:flex lg:items-center lg:w-auto"] $ do
       div_ [class_ "text-md lg:mx-auto"] $ do
-        a_ [href_ "#", class_"block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"] "Course Listing"
+        a_ [href_ "/", class_"block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"] "Home"
         a_ [href_ "#", class_"block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"] "Test"
       div_ $ do
-        a_ [href_ "#", class_ "inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"] "Github"
+        a_ [href_ "https://github.com/dcao/seascape", class_ "inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"] "Github"
 
 defaultPartial :: String -> Html () -> Html ()
 defaultPartial t body =
@@ -42,3 +42,6 @@ defaultPartial t body =
     body_ [class_ "font-sans"] $ do
       navbarPartial
       body
+    p_ [class_ "text-center text-gray-500 text-xs mt-6"] $ do
+      toHtmlRaw ("&copy; 2019 David Cao, Tung Doan. Born at " :: Text)
+      a_ [href_ "https://sites.google.com/a/eng.ucsd.edu/spis/", class_ "text-teal-600"] "SPIS 2019."
